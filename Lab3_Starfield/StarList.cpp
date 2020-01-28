351a1919ced2a549ff66bce11bc09e768a38f511
lweldon <lweldon@ccs.neu.edu>
2020-01-28 15:39:14 -0800

    no wait this is the correct way

--- Lab3_Starfield/StarList.cpp
+++ Lab3_Starfield/StarList.cpp
@@ -40,7 +40,7 @@ void StarList::updateAndRender(QImage& image, float delta, const QSize& windowSi
 
     // Note the conversion to radians
     // TODO: Modify me
-    float tanHalfFOV = tan(1.22173f);
+    float tanHalfFOV = tan(0.610865);
 
     // Iterate through all of your stars 
     for (int i = 0; i < stars_.size(); i++) {
